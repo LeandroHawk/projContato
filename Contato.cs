25a6a77a6b6a87195ea8d6f19fbbbe257ffdb1b7
Leandro Felix <107886470+LeandroHawk@users.noreply.github.com>
2024-10-13 18:31:21 -0300

    Add files via upload

---
+++ Contato.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjContato
+{
+    public class Contato
+    {
+        private string email { get; set; }
+        private string nome { get; set; }
+        private Data DtNasc { get; set; }
+        private List<Telefone> telefones;
+
+        //Construtor
+        public Contato()
+        {
+            this.telefones = new List<Telefone>();
+        }
+
+        public string Email { get => email; set => email = value; }
+        public string Nome { get => nome; set => nome = value; }
+        public Data dtNasc { get => dtNasc; set => dtNasc = value; }
+        public List<Telefone> Telefones { get => telefones; set => telefones = value; }
+
+        public int GetIdade()
+        {
+            DateTime now = DateTime.Now;
+            int idade = now.Year - dtNasc.Ano;
+
+            if (now.Month < dtNasc.Mes || (now.Month == dtNasc.Mes && now.Day < dtNasc.Dia))
+            {
+                idade--;
+            }
+            return idade;
+        }
+
+        public void addTelefone(Telefone t)
+        {
+            telefones.Add(t);
+        }
+
+        public string getTelefonePrincipal()
+        {
+            foreach (Telefone t in telefones)
+            {
+                if (t.Principal)
+                {
+                    return t.Numero;
+                }
+            }
+            return null;
+        }
+
+        public override string ToString()
+        {
+            return $"{Nome} - {Email} - {DtNasc} - {getTelefonePrincipal()}";
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || obj.GetType() != GetType())
+            {
+                return false;
+            }
+            Contato c = (Contato)obj;
+            return email.Equals(c.email);
+        }
+
+
+
+    }
+
+}
---
+++ Data.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjContato
+{
+    public class Data
+    {
+        private int dia;
+        private int mes;
+        private int ano;
+
+        public int Dia { get => dia; set => dia = value; }
+        public int Mes { get => mes; set => mes = value; }
+        public int Ano { get => ano; set => ano = value; }
+
+        //Construtor 
+        public Data(int dia, int mes, int ano)
+        {
+            this.dia = dia;
+            this.mes = mes;
+            this.ano = ano;
+        }
+
+        public Data()
+        {
+        }
+
+        //Método setter
+        public void setData(int dia, int mes, int ano)
+        {
+            this.dia = dia;
+            this.mes = mes;
+            this.ano = ano;
+        }
+
+        // formato "dd/mm/aaaa"
+        public override string ToString()
+        {
+            return $"{dia:D2}/{mes:D2}/{ano:D4}";
+        }
+    }
+
+
+
+
+}
